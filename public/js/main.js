fdb76fab2235f613019ac9ac9932e8a47db0a3af
David Sharpe <davidasharpe@me.com>
2017-03-22 10:15:50 +1100

    Update main.js

--- public/js/main.js
+++ public/js/main.js
@@ -6,13 +6,13 @@ $(document).ready(function(){
   var addButton = $('.add_button'); //Add button selector
   var director = $('.director'); //Input field wrapper
 
-  var fieldHTML = '<div class="field">';
-      fieldHTML += '<a href="javascript:void(0);" class="remove_button"><span class="glyphicon glyphicon-minus-sign" aria-hidden="true"></span></a>';
-      fieldHTML += '<select class="form-control">';
-      fieldHTML += '<option value="select">select</option>';
-      fieldHTML += '</select>';
-      fieldHTML += '</div>';
+  var fieldHTML = '<div class="field">' + 
+                  '<a href="javascript:void(0);" class="remove_button"><span class="glyphicon glyphicon-minus-sign" aria-hidden="true"></span></a>' +
+                  '<select class="form-control">' +
+                  '<option value="select">select</option>';
+             
 
+  $
   var x = 1; //Initial field counter is 1
 
 /*
@@ -23,21 +23,34 @@ $(document).ready(function(){
       }
   });
 */
-
+  
 // Add director field
 
   $('.director').on('click', '.add_button', function(e) { //Once add button is clicked
     if(x < maxField){ //Check maximum number of input fields
       x++; //Increment field counter
-      $(this).parent('div').append(fieldHTML); // Add field html
+      
+      $.ajax({      
+        method: 'GET', 
+        url: 'get_directors.php'
+        }).done.(function(data){
+        var result = $.parseJSON(data);
+         $.each(result, function(key, value){
+         fieldHTML += '<option>' + value['FirstName'] + value['LastName'] + '</option>';
+         }
+         fieldHTML += '</select></div>';
+         $(this).parent('div').append(fieldHTML); // Add field html
+       });  
     }
   });
 
+  /*
   $('.director').on('click', '.remove_button', function(e){ //Once remove button is clicked
       e.preventDefault();
       $(this).parent('div').remove(); //Remove field html
       x--; //Decrement field counter
   });
+*/
 
 // Add producer field
 
