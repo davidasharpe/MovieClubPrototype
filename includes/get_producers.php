605407fc23996365ee334437a7c92dd20596d378
David Sharpe <davidasharpe@me.com>
2017-03-23 16:17:56 +1100

    Update get_producers.php

--- includes/get_producers.php
+++ includes/get_producers.php
@@ -1,14 +1,22 @@
-<?php  require_once('database.php');  
-require_once('functions.php');  
-$result_array = array();  
-$query = select_2col('directors', 'FirstName', 'LastName', 'FirstName');  
-$result = mysqli_query($connection, $query);  
-if (mysqli_num_rows($result) > 0){    
-while ($row = mysqli_fetch_assoc($result)){      
-array_push($result_array, $row);    
-}  
-}/* send a JSON encded array to client */  
-echo json_encode($result_array);  
+<?php  
+require_once('database.php');  
+require_once('functions.php');
+
+$result_array = array();
+
+$query = select_2col('producers', 'FirstName', 'LastName', 'FirstName');
+
+$result = mysqli_query($connection, $query);
+
+if (mysqli_num_rows($result) > 0){  
+  
+  while ($row = mysqli_fetch_assoc($result)){      
+  array_push($result_array, $row);    
+  }  
+}/* send a JSON encded array to client */
+
+echo json_encode($result_array);
+
 mysqli_close($connection);
 
 ?>
