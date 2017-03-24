9684460682d60795ce26891c1ac90d695b027e9b
David Sharpe <davidasharpe@me.com>
2017-03-24 14:03:53 +1100

    Update fetch.js

--- sandbox/fetch.js
+++ sandbox/fetch.js
@@ -1,1 +1,9 @@
-$('#brand').change(function(){ $.getJSON(  'fetch.php',  'brand='+$('#brand').val(),  function(result){   $('#item').empty();   $.each(result.result, function(){    $('#item').append('<option>'+this['item']+'</option>');   });  } );});
+$('#brand').change(function(){ 
+  $.getJSON('fetch.php', 'brand='+$('#brand').val(), function(result){   
+    $('#item').empty();   
+    $.each(result.result, function(){    
+      $('#item').append('<option>'+this['item']+'</option>');   +
+    });  
+  });
+});
+
