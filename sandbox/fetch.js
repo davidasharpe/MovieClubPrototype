28c4eea293a5602745cfd098601b8a0801cf549a
David Sharpe <davidasharpe@me.com>
2017-03-24 13:45:35 +1100

    Create fetch.js

---
+++ sandbox/fetch.js
@@ -0,0 +1,1 @@
+$('#brand').change(function(){ $.getJSON(  'fetch.php',  'brand='+$('#brand').val(),  function(result){   $('#item').empty();   $.each(result.result, function(){    $('#item').append('<option>'+this['item']+'</option>');   });  } );});
