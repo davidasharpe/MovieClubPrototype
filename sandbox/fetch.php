81409799c2f4cb112ff13e8ce8e3f83e1e0947f6
David Sharpe <davidasharpe@me.com>
2017-03-24 14:36:16 +1100

    Update fetch.php

--- sandbox/fetch.php
+++ sandbox/fetch.php
@@ -5,11 +5,14 @@ define('PASSWORD',''); 
 define('DB','myDatabase');  
 $con = mysqli_connect(HOST,USERNAME,PASSWORD,DB);  
 $brand = $_GET['brand'];  
-$sql = "select item from products where brand='$brand'";  
-$res = mysqli_query($con,$sql);  $result = array();  
+$sql = "SELECT item FROM products WHERE brand='$brand'";  
+$res = mysqli_query($con,$sql);  
+$result = array();  
 while($row = mysqli_fetch_array($res)){  
   array_push($result, array('item'=>$row[0])); 
 }  
 echo json_encode(array('result'=>$result));  
 mysqli_close($con);
 ?>
+
+
