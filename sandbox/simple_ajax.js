37599436be6c828bc548f4bf3b8a60506e319c7d
David Sharpe <davidasharpe@me.com>
2017-03-24 11:29:42 +1100

    Update simple_ajax.js

--- sandbox/simple_ajax.js
+++ sandbox/simple_ajax.js
@@ -1,7 +1,35 @@
+// http://thisinterestsme.com/simple-ajax-request-example-jquery-php/
+
+// Ajax GET request
 $.ajax({    
-type: "GET",    
-url: 'test.php',    
-success: function(data){        
-alert(data);    
-}
+  type: "GET",    
+  url: 'test.php',    
+  success: function(data){        
+    alert(data);    
+  }
 });
+
+// Ajax GET request with query string parameters
+$.ajax({    
+  type: "GET",    
+  url: 'test.php',    
+  data: {name: 'Wayne'},    
+  success: function(data){        
+    alert(data);    
+  }
+});
+
+// Ajax POST request
+$.ajax({    
+  type: "POST",    
+  url: 'submission.php',    
+  data: {name: 'Wayne', age: 27},    
+  success: function(data){        
+    alert(data);    
+  }
+});
+
+
+
+
+
